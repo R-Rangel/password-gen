2e415d44da06fe29816bfe9822d85eb6df284428
R-Rangel <r.rangel.0222@gmail.com>
2023-02-13 15:10:18 -0600

    js

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,11 @@
-  if(passwordLength < 8){
-      alert('Password length must be at least 8 characters');
-    }
-    
+const generateBtn = document.querySelector("#generate");
+const passwordText = document.querySelector("#password");
 
-  var hasNumeric = confirm("Click OK to confirm including numeric characters.");
-  var hasLower = confirm("Click OK to confirm including Lower characters.");
-  var hasUpper = confirm("Click OK to confirm including Upper characters.");
-  var hasSpecial = confirm("Click OK to confirm including Special characters.");
-
-  if(hasNumeric === false && hasLower === false && hasUpper === false && hasSpecial === false){
-      alert('Must select at least one character type');
+generateBtn.addEventListener("click", function() {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+  let password = "";
+  for (let i = 0; i < 8; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
   }
-
-  var userPassword = {
-   length: passwordLength,
-   hasNumeric: hasNumeric,
-   hasLower: hasLower,
-   hasUpper: hasUpper,
-   hasSpecial: hasSpecial,
-   return: userPassword,}
-
-  
-  var userOptions = getOptions();
-  var possibleChars = [];
-  var result = [];
-  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  if(userOptions.hasNumeric === true){
-  possibleChars = possibleChars.concat(numericCharacters);
-  }
-  
-
- generateBtn.addEventListener("click", writePassword);
+  passwordText.value = password;
+});
